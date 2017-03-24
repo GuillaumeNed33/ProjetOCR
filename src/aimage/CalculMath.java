f4975e317b11325a199f9f49bac001c25ad4e308
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-03-24 12:10:04 +0100

    ajout des fonctions de calculMath

---
+++ src/aimage/CalculMath.java
@@ -0,0 +1,30 @@
+package aimage;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Thomas on 24/03/2017.
+ */
+public class CalculMath {
+
+    public static double distEucli( ArrayList<Double>vect1,ArrayList <Double>vect2) {
+        double res = Double.MAX_VALUE;
+        for(int i = 0; i < vect1.size(); i++) {
+            res += Math.pow(vect1.get(i) - vect2.get(i),2);
+        }
+        return Math.sqrt(res);
+    }
+
+    public static int PPV(ArrayList<Double>vect,ArrayList<ArrayList<Double> >tabVect,int except) throws ArrayIndexOutOfBoundsException {
+        int pos = 0;
+        for(int i = 0; i<tabVect.size();i++) {
+            if(tabVect.get(i).size() != vect.size()) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            if(i != except && distEucli(vect,tabVect.get(i)) < distEucli(vect,tabVect.get(pos))) {
+                pos = i;
+            }
+        }
+        return pos;
+    }
+}
--- src/aimage/Image.java
+++ src/aimage/Image.java
@@ -1,7 +1,5 @@
 package aimage;
 
-/**
- * Created by Guillaume on 24/03/2017.
- */
 public class Image {
+
 }
